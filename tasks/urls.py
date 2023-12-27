51ea3ec4d724bf37abd4cd77ed2dddd60638aa58
Chozzer <92675566+Chozzer@users.noreply.github.com>
2023-12-27 23:51:04 +0000

    Moved task functions to new app called tasks added edit task and delete task functionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,6 +6,10 @@ import tasks
 
 
 urlpatterns = [
-    path("add_task/<int:parent>", tasks.add_task, name='add_task'),
+    path("add_task/<int:parent>", tasks.views.add_task, name='add_task'),
+    path("display_task/<int:req_task>", tasks.views.display_task, name="display_task"),
+    path("edit_task/<int:req_task>", tasks.views.edit_task, name="edit_task"),
+    path("delete_task/<int:req_task>", tasks.views.delete_task, name="delete_task"),
+    
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -24,10 +24,49 @@ def add_task(request, parent):
                                         order=next_order,
                                         status_id=pstatus.id)
             newtask.save
+            projectid=parent
             return redirect('home')
             
         else:
             pstatus= status.objects.all()
             return render(request, 'add_task.html', {'status':pstatus})
     else:
+        print("not logged in")
         return redirect('home')
+    
+
+
+def display_task(request, req_task):
+    if request.user.is_authenticated:
+        task_to_display = task.objects.get(pk=req_task)
+        return render(request, 'display_task.html', {'task':task_to_display})
+    
+    else:
+        return redirect('home')
+    
+def edit_task(request, req_task):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            id = req_task
+            pstatus = request.POST["status"]
+            pstatus=status.objects.get(status=pstatus)
+            edited = task.objects.get(id=id)
+            edited.title = request.POST["title"]
+            edited.task = request.POST["task"]
+            edited.order = request.POST["order"]
+            edited.status_id = pstatus.id
+            edited.save()
+            return redirect("home")
+
+        else:
+            task_to_edit = task.objects.get(pk=req_task)
+            pstatus= status.objects.all()
+            return render(request, 'edit_task.html', {'task':task_to_edit, 'status':pstatus})
+    
+    else:
+        return redirect('home')
+
+
+def delete_task(request, req_task):
+    task.objects.filter(id=req_task).delete()
+    return redirect('home')
